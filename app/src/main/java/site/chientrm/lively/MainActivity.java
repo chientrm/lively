8cdeafa02f96b825c4c14472812203186cd2f5f6
chientrm <chientrm@gmail.com>
2022-11-29 10:22:50 +0000

    remove gradle project

--- app/src/main/java/site/chientrm/lively/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package site.chientrm.lively;
-
-import android.app.Activity;
-
-public class MainActivity extends Activity {
-
-}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<svelte:head>
+	<title>Lively</title>
+    <meta property="og:title" content="How to Become an SEO Expert (8 Steps)" />
+    <meta property="og:description" content="Get from SEO newbie to SEO pro in 8 simple steps." />
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
