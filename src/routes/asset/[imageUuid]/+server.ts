0a9461dcfcc4207453e0e4050722ed25ab9260a4
chientrm <chientrm@gmail.com>
2022-12-01 12:26:22 +0000

    fix: cache asset

--- src/routes/asset/[imageUuid]/+server.ts
+++ src/routes/asset/[imageUuid]/+server.ts
@@ -1,11 +1,12 @@
 import type { RequestHandler } from './$types';
 import fs from 'fs';
 
-const GET: RequestHandler = async ({ request, params, locals }) => {
+const GET: RequestHandler = async ({ request, params, locals, setHeaders }) => {
 	const { email } = locals.user!,
 		{ imageUuid } = params,
 		imagePath = `/home/ubuntu/blob/lively/${email}/${imageUuid}`,
 		image = fs.readFileSync(imagePath);
+	setHeaders({ 'cache-control': 'public, max-age=31536000;' });
 	return new Response(image);
 };
 
