79f1115b2bdd1e8dbf22c8a7fd186cd4fb578ad2
chientrm <chientrm@gmail.com>
2022-12-01 15:40:25 +0000

    fix: remove cjs plugin

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { imagetools } from 'vite-imagetools';
-import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [viteCommonjs(), imagetools(), sveltekit()],
-	optimizeDeps: {
-		esbuildOptions: {
-			plugins: [
-				esbuildCommonjs(['sharp'])
-			]
-		}
-	}
+	plugins: [imagetools(), sveltekit()],
 };
 
 export default config;
