2483ecc962d13822f3e9f4e4cf7514d87be56315
chientrm <chientrm@gmail.com>
2022-12-01 15:08:57 +0000

    fix: __dirname

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,16 @@
 import cookie from '$lib/constants/cookie';
 import { decrypt } from '$lib/helpers/crypt';
 import type { Handle } from '@sveltejs/kit';
+import { dirname } from 'path';
+
+if (!global.__dirname) {
+	const dir = dirname(import.meta.url);
+	Object.defineProperty(global, '__dirname', {
+		// @ts-ignore
+		__proto__: null,
+		get: () => dir
+	});
+}
 
 const handle: Handle = async ({ event, resolve }) => {
 	const cookieUser = event.cookies.get(cookie.USER);
