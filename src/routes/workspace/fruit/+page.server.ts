d771dcb219af4ac017607b10009b74c33c2251c8
chientrm <chientrm@gmail.com>
2022-12-02 06:12:35 +0000

    fix

--- src/routes/workspace/fruit/+page.server.ts
+++ src/routes/workspace/fruit/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from './$types';
 
 const load: PageServerLoad = async ({ locals }) => {
 	const { email: userEmail } = locals.user!,
-		fruits = await prisma.dish.findMany({
+		fruits = await prisma.fruit.findMany({
 			select: { id: true, name: true, imageUuid: true },
 			where: { userEmail }
 		});
